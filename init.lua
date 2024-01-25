d6c882388134bd055f8e7208698c61f0e98a5785
Awadhy Mohammed <awadhym@outlook.com>
2024-01-25 13:42:04 +0000

    Set up Lazy.nvim plugin manager

---
+++ init.lua
@@ -0,0 +1,21 @@
+vim.cmd.colorscheme('default')
+
+-- Lazy.nvim Plugin Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim" -- define local variable that is essentially a path. .. means concatenation in lua
+if not vim.loop.fs_stat(lazypath) then -- if path does not exist on file then...
+  vim.fn.system({						-- install lazy.nvim
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath) -- add the path to the front of the vim run time path. (List of paths accessible to neovim)
+
+require('lazy').setup({
+  {'rebelot/kanagawa.nvim'},
+})
+
+vim.cmd.colorscheme('kanagawa')
