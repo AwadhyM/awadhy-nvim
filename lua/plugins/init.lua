2e87c49143e1d7bef75e97d5f76200c5f3d95b9d
Awadhy Mohammed <awadhym@outlook.com>
2024-01-28 19:47:45 +0000

    Set up LSP for C/C++ using Clangd and Neovim-lspconfig

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,25 @@
 require('lazy').setup({
-  {'rebelot/kanagawa.nvim'},
+  {'rebelot/kanagawa.nvim',
+	config = function()
+		vim.cmd 'colorscheme kanagawa-wave'
+	end,
+  },
 
   -- Treesitter
   {'nvim-treesitter/nvim-treesitter',
    	config = function()
     require "plugins.configs.treesitter"
 	end,},
+   -- LSP
+   {'neovim/nvim-lspconfig',
+	config = function()
+		local lspconfig = require("lspconfig")
+		lspconfig.clangd.setup({
+			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0}),
+			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0}),
+			vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer = 0}),
+		})
+	end,
+	},
 })
 
