963a1156a720b535ade5a514154da9f6b5f4c6a6
Awadhy Mohammed <awadhym@outlook.com>
2024-02-04 13:36:45 +0000

    Add additional key bindings for Clangd LSP config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -18,6 +18,8 @@ require('lazy').setup({
 			vim.keymap.set('n', 'K', vim.lsp.buf.hover, {buffer = 0}),
 			vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {buffer = 0}),
 			vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, {buffer = 0}),
+			vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {buffer = 0}),
+			vim.keymap.set('n', '<leader>df', vim.diagnostic.goto_next, {buffer = 0}),
 		})
 	end,
 	},
