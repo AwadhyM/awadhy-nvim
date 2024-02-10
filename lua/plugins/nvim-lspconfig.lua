03558c773a07f55e9c8b6f67d4a3378fedbb3b82
Awadhy Mohammed <awadhym@outlook.com>
2024-02-10 21:34:50 +0000

    Set up nvim autocomplete and snippets

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -1,10 +1,11 @@
 return {
 	'neovim/nvim-lspconfig',
 	config = function()
+		local capabilities = require('cmp_nvim_lsp').default_capabilities()
 		local lspconfig = require("lspconfig")
-		lspconfig.clangd.setup({})
-		lspconfig.pyright.setup({})
-		lspconfig.lua_ls.setup({
+		lspconfig.clangd.setup({ capabilities = capabilities})
+		lspconfig.pyright.setup({ capabilities = capabilities})
+		lspconfig.lua_ls.setup({ capabilities = capabilities,
 			settings = {
 					Lua = {
 						diagnostics = {
