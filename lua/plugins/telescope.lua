d020aa2a88cd69f2b5f6a172cda743a0cd01e001
Awadhy Mohammed <awadhym@outlook.com>
2025-02-02 17:06:18 +0000

    Revisit configuration for telescope and improve workflow
    - Make sure Telescope uses fzf dependency
    - Improve understanding of Telescope and how it can be configured
    - Set key map for live_grep. Test that words can be copy and pasted
      directly into live_grep
    - Understand usage of grep_string

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,11 +1,35 @@
 return {
-	'nvim-telescope/telescope.nvim', tag = '0.1.5',
-	dependencies = {'nvim-lua/plenary.nvim'},
+	'nvim-telescope/telescope.nvim', tag = '4.1.5',
+    dependencies = {
+    	'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
 	config = function()
+		require('telescope').setup{
+			defaults = {
+				prompt_prefix = "$ ",
+			},
+		}
 		local builtin = require("telescope.builtin")
 		vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
 		vim.keymap.set("n", "<leader>gf", builtin.git_files, {})
 		vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
-		vim.keymap.set("n", "<leader>ps",function() builtin.grep_string({ search = vim.fn.input("Grep > ")}); end)local builtin = require('telescope.builtin')
-	end
+		vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+		vim.keymap.set("n", "<leader>gs",function() builtin.grep_string({ search = vim.fn.input("Grep > ")}); end)local builtin = require('telescope.builtin')
+
+
+		-- The code below is a potential alternative to searching within buffer.
+		-- -- Map '/' to start current_buffer_fuzzy_find with custom options
+		-- vim.keymap.set("n", "/", function()
+		--   builtin.current_buffer_fuzzy_find({
+		-- 	theme = "ivy",  -- Use the 'ivy' theme
+		-- 	sorting_strategy = "ascending",  -- Sort results in ascending order
+		-- 	layout_config = {
+		-- 	  prompt_position = "top",  -- Place the prompt at the top
+		-- 	},
+		--   })
+		-- end, { noremap = true, silent = true })
+
+		require('telescope').load_extension('fzf')
+	end,
 }
